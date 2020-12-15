c343b49ca7a90e5a62c16285824bb03fa6c697fa
ivanjjj <ij.jennings@yahoo.com>
2020-12-16 12:04:32 +1300

    First commit - plot 1 completed.

---
+++ R/plot1code.R
@@ -0,0 +1,25 @@
+library(dplyr)
+
+file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(file_url, "my_data.zip", method = "curl")
+unzip("my_data.zip",exdir = "my_data")
+
+summarySCC_PM25 <- readRDS(file.path(getwd(), "my_data/summarySCC_PM25.rds"))
+Source_Classification_Code <- readRDS(file.path(getwd(), "my_data/Source_Classification_Code.rds"))
+
+plot1_data = summarySCC_PM25 %>%
+  group_by(year) %>%
+  summarise(Sum = sum(Emissions, na.rm=TRUE))
+
+
+with(plot1_data, plot(year,Sum/1000000,
+                      ylab = "Total PM2.5 Emissions (Millions)",
+                      xlab = "Year",
+                      title(main = "Total US PM2.5 Emissions 1998 - 2008"),
+                      axes = F,
+                      pch = 1
+                      ))
+box()
+axis(2)
+axis(1, at = plot1_data$year)
+
